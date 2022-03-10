101692a087080ed9f84827d7f00a191d5f17241e
J0K3Rn <46616635+J0K3Rn@users.noreply.github.com>
2022-03-09 20:46:38 -0600

    Update models.py

--- Website/src/members/models.py
+++ Website/src/members/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-	first_name = models.TextField()
-	last_name = models.TextField()
-	email = models.EmailField()
-	password = models.TextField()
-	bill_address = models.TextField()
-	points = models.IntegerField()
-	pref_payment = models.TextField()
+	id = models.BigAutoField(primary_key=True)
+	username = models.TextField(max_length=128)
+	password = models.TextField(max_length=128)
+	address_1 = models.CharField(_("Address"), max_length=128)
+	address_2 = models.CharField(_("Address Cont'd"), max_length=128, blank=True)
+	city = models.CharField(_("City"), max_length=64, default="")
+	state = models.CharField(_("State"), default="OH")
+	zip_code = models.CharField(_("Zip Code"), max_length=5, default="")
+	profit_margin = models.DecimalField(decimal_places=2)
